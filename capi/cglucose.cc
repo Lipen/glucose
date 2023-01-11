b5bb24caf05674fc32872a535c2948c43c522b08
Konstantin Chukharev <lipen00@gmail.com>
2023-01-11 21:19:56 +0400

    Reorganize C API

--- capi/cglucose.cc
+++ capi/cglucose.cc
@@ -39,21 +39,29 @@ glucose_lbool glucose_get_l_True(void) { return glucose_l_True; }
 glucose_lbool glucose_get_l_False(void) { return glucose_l_False; }
 glucose_lbool glucose_get_l_Undef(void) { return glucose_l_Undef; }
 
-// Solver C-API wrapper functions:
-//
 const char *glucose_signature(void) { return "glucose"; }
 CGlucose *glucose_init(void) { return new CGlucose(); }
 void glucose_release(CGlucose *s) { delete s; }
+
 glucose_Var glucose_newVar(CGlucose *s) { return s->newVar(); }
 glucose_Lit glucose_newLit(CGlucose *s) { return toInt(mkLit(s->newVar())); }
+
 glucose_Lit glucose_mkLit(glucose_Var x) { return toInt(mkLit(x)); }
 glucose_Lit glucose_mkLit_args(glucose_Var x, int sign) { return toInt(mkLit(x, sign)); }
 glucose_Lit glucose_negate(glucose_Lit p) { return toInt(~toLit(p)); }
 glucose_Var glucose_var(glucose_Lit p) { return var(toLit(p)); }
 bool glucose_sign(glucose_Lit p) { return sign(toLit(p)); }
+
+void glucose_setDecisionVar(CGlucose *s, glucose_Var v, bool b) { s->setDecisionVar(v, b); }
+void glucose_setPolarity(CGlucose *s, glucose_Var v, bool b) { s->setPolarity(v, b); }
+void glucose_setFrozen(CGlucose *s, glucose_Var v, bool b) { s->setFrozen(v, b); }
+bool glucose_isEliminated(CGlucose *s, glucose_Var v) { return s->isEliminated(v); }
+bool glucose_eliminate(CGlucose *s, bool turn_off_elim) { return s->eliminate(turn_off_elim); }
+
 void glucose_addClause_begin(CGlucose *s) { s->clause.clear(); }
 void glucose_addClause_addLit(CGlucose *s, glucose_Lit p) { s->clause.push(toLit(p)); }
 bool glucose_addClause_commit(CGlucose *s) { return s->addClause_(s->clause); }
+
 bool glucose_simplify(CGlucose *s) { return s->simplify(); }
 
 // NOTE: Currently these run with default settings for implicitly calling preprocessing. Turn off
@@ -64,37 +72,36 @@ bool glucose_solve_commit(CGlucose *s) { return s->solve(s->assumps); }
 glucose_lbool glucose_limited_solve_commit(CGlucose *s) { return toC(s->solveLimited(s->assumps)); }
 
 bool glucose_okay(CGlucose *s) { return s->okay(); }
-void glucose_setPolarity(CGlucose *s, glucose_Var v, bool b) { s->setPolarity(v, b); }
-void glucose_setDecisionVar(CGlucose *s, glucose_Var v, bool b) { s->setDecisionVar(v, b); }
+
 glucose_lbool glucose_value_Var(CGlucose *s, glucose_Var x) { return toC(s->value(x)); }
 glucose_lbool glucose_value_Lit(CGlucose *s, glucose_Lit p) { return toC(s->value(toLit(p))); }
 glucose_lbool glucose_modelValue_Var(CGlucose *s, glucose_Var x) { return toC(s->modelValue(x)); }
 glucose_lbool glucose_modelValue_Lit(CGlucose *s, glucose_Lit p) { return toC(s->modelValue(toLit(p))); }
+
+int glucose_conflict_len(CGlucose *s) { return s->conflict.size(); }
+glucose_Lit glucose_conflict_nthLit(CGlucose *s, int i) { return toInt(s->conflict[i]); }
+
+void glucose_set_conf_budget(CGlucose *s, int x) { s->setConfBudget(x); }
+void glucose_set_prop_budget(CGlucose *s, int x) { s->setPropBudget(x); }
+void glucose_no_budget(CGlucose *s) { s->budgetOff(); }
+
+void glucose_interrupt(CGlucose *s) { s->interrupt(); }
+void glucose_clearInterrupt(CGlucose *s) { s->clearInterrupt(); }
+
+void glucose_setIncremental(CGlucose *s) { s->setIncrementalMode(); }
+bool glucose_isIncremental(CGlucose *s) { return s->isIncremental(); }
+void glucose_set_verbosity(CGlucose *s, int v) { s->verbosity = v; }
+int glucose_get_verbosity(CGlucose *s) { return s->verbosity; }
+
 int glucose_num_assigns(CGlucose *s) { return s->nAssigns(); }
 int glucose_num_clauses(CGlucose *s) { return s->nClauses(); }
 int glucose_num_learnts(CGlucose *s) { return s->nLearnts(); }
 int glucose_num_vars(CGlucose *s) { return s->nVars(); }
 int glucose_num_freeVars(CGlucose *s) { return s->nFreeVars(); }
-int glucose_conflict_len(CGlucose *s) { return s->conflict.size(); }
-glucose_Lit glucose_conflict_nthLit(CGlucose *s, int i) { return toInt(s->conflict[i]); }
-void glucose_set_verbosity(CGlucose *s, int v) { s->verbosity = v; }
-int glucose_get_verbosity(CGlucose *s) { return s->verbosity; }
 int64_t glucose_num_conflicts(CGlucose *s) { return (int64_t)s->conflicts; }
 int64_t glucose_num_decisions(CGlucose *s) { return (int64_t)s->decisions; }
 int64_t glucose_num_restarts(CGlucose *s) { return (int64_t)s->starts; }
 int64_t glucose_num_propagations(CGlucose *s) { return (int64_t)s->propagations; }
-void glucose_set_conf_budget(CGlucose *s, int x) { s->setConfBudget(x); }
-void glucose_set_prop_budget(CGlucose *s, int x) { s->setPropBudget(x); }
-void glucose_no_budget(CGlucose *s) { s->budgetOff(); }
-
-// Resource constraints:
-void glucose_interrupt(CGlucose *s) { s->interrupt(); }
-void glucose_clearInterrupt(CGlucose *s) { s->clearInterrupt(); }
-
-// SimpSolver methods:
-void glucose_setFrozen(CGlucose *s, glucose_Var v, bool b) { s->setFrozen(v, b); }
-bool glucose_isEliminated(CGlucose *s, glucose_Var v) { return s->isEliminated(v); }
-bool glucose_eliminate(CGlucose *s, bool turn_off_elim) { return s->eliminate(turn_off_elim); }
 
 // Convenience functions for actual c-programmers (not language interfacing people):
 //
--- capi/cglucose.h
+++ capi/cglucose.h
@@ -16,20 +16,31 @@ extern const glucose_lbool glucose_l_True;
 extern const glucose_lbool glucose_l_False;
 extern const glucose_lbool glucose_l_Undef;
 
+glucose_lbool glucose_get_l_True(void);
+glucose_lbool glucose_get_l_False(void);
+glucose_lbool glucose_get_l_Undef(void);
+
 const char *glucose_signature(void);
 CGlucose *glucose_init(void);
 void glucose_release(CGlucose *s);
 
+// Variables/Literals:
 glucose_Var glucose_newVar(CGlucose *s);
 glucose_Lit glucose_newLit(CGlucose *s);
 
 glucose_Lit glucose_mkLit(glucose_Var x);
 glucose_Lit glucose_mkLit_args(glucose_Var x, int sign);
 glucose_Lit glucose_negate(glucose_Lit p);
-
 glucose_Var glucose_var(glucose_Lit p);
 bool glucose_sign(glucose_Lit p);
 
+void glucose_setPolarity(CGlucose *s, glucose_Var v, bool b);
+void glucose_setDecisionVar(CGlucose *s, glucose_Var v, bool b);
+void glucose_setFrozen(CGlucose *s, glucose_Var v, bool b);
+bool glucose_isEliminated(CGlucose *s, glucose_Var v);
+bool glucose_eliminate(CGlucose *s, bool turn_off_elim);
+
+// Clause:
 bool glucose_addClause(CGlucose *s, int len, glucose_Lit *ps);
 void glucose_addClause_begin(CGlucose *s);
 void glucose_addClause_addLit(CGlucose *s, glucose_Lit p);
@@ -46,23 +57,10 @@ glucose_lbool glucose_limited_solve_commit(CGlucose *s);
 
 bool glucose_okay(CGlucose *s);
 
-void glucose_setPolarity(CGlucose *s, glucose_Var v, bool b);
-void glucose_setDecisionVar(CGlucose *s, glucose_Var v, bool b);
-
-glucose_lbool glucose_get_l_True(void);
-glucose_lbool glucose_get_l_False(void);
-glucose_lbool glucose_get_l_Undef(void);
-
 glucose_lbool glucose_value_Var(CGlucose *s, glucose_Var x);
 glucose_lbool glucose_value_Lit(CGlucose *s, glucose_Lit p);
 glucose_lbool glucose_modelValue_Var(CGlucose *s, glucose_Var x);
 glucose_lbool glucose_modelValue_Lit(CGlucose *s, glucose_Lit p);
-
-int glucose_num_assigns(CGlucose *s);
-int glucose_num_clauses(CGlucose *s);
-int glucose_num_learnts(CGlucose *s);
-int glucose_num_vars(CGlucose *s);
-int glucose_num_freeVars(CGlucose *s);
 
 int glucose_conflict_len(CGlucose *s);
 glucose_Lit glucose_conflict_nthLit(CGlucose *s, int i);
@@ -71,21 +69,19 @@ void glucose_set_conf_budget(CGlucose *s, int x);
 void glucose_set_prop_budget(CGlucose *s, int x);
 void glucose_no_budget(CGlucose *s);
 
-// Resource constraints:
 void glucose_interrupt(CGlucose *s);
 void glucose_clearInterrupt(CGlucose *s);
 
-// SimpSolver methods:
-void glucose_setFrozen(CGlucose *s, glucose_Var v, bool b);
-bool glucose_isEliminated(CGlucose *s, glucose_Var v);
-bool glucose_eliminate(CGlucose *s, bool turn_off_elim);
-
-// Setters:
-
+void glucose_setIncremental(CGlucose *s);
+bool glucose_isIncremental(CGlucose *s);
 void glucose_set_verbosity(CGlucose *s, int v);
+int glucose_get_verbosity(CGlucose *s, int v);
 
-// Getters:
-
+int glucose_num_assigns(CGlucose *s);
+int glucose_num_clauses(CGlucose *s);
+int glucose_num_learnts(CGlucose *s);
+int glucose_num_vars(CGlucose *s);
+int glucose_num_freeVars(CGlucose *s);
 int64_t glucose_num_conflicts(CGlucose *s);
 int64_t glucose_num_decisions(CGlucose *s);
 int64_t glucose_num_restarts(CGlucose *s);
